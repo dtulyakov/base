cccb3fd7600f5a0289d86d9070fa6ba7ecb54905
Денис Туляков <dtulyakov@gmail.com>
2017-12-13 20:39:29 +0300

    На ветке master
     Начальный коммит
    
     Изменения, которые будут включены в коммит:
    	новый файл:    .gitignore
    	новый файл:    README.md
    	новый файл:    install.sh

---
+++ README.md
@@ -0,0 +1,6 @@
+## Install
+
+```bash
+
+bash <(curl -Ss https://raw.githubusercontent.com/dtulyakov/base/master/install.sh)
+```
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!//usr/bin/env sh
+
+######## SET
+FDATE=$(/bin/date '+%Y/%m/%d')
+set -x
+######## SCRIPTS
+sudo apt update -qq
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -yqq \
+  && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
+  && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+  && sudo apt update -qq \
+  && sudo apt install -yqq \
+     docker-ce \
+     bridge-utils \
+     zsh \
+     nmon \
+     htop \
+     bmon \
+     nload \
+     vlan \
+     jq \
+     ccze \
+     cowsay \
+     rsync \
+     git \
+     linux-image-extra-$(uname -r) \
+     linux-image-extra-virtual \
+  && sudo usermod -aG docker $USER
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
+  && sudo chmod +x /usr/local/bin/docker-compose
+
+#  && sudo apt-add-repository ppa:ansible/ansible -y
