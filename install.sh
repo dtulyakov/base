5d4cbe1fe603d8e3ca136d7fc04524bd46e1ed5b
Денис Туляков <dtulyakov@gmail.com>
2017-12-13 20:47:57 +0300

    На ветке master
     Изменения, которые будут включены в коммит:
    	изменено:      install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,10 @@ set -x
 sudo apt update -qq
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -yqq \
   && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
+  && curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo apt-key add - \
+  && curl -fsSL https://www.virtualbox.org/download/oracle_vbox.asc | sudo apt-key add - \
   && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+  && sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" \
   && sudo apt update -qq \
   && sudo apt install -yqq \
      docker-ce \
@@ -24,11 +27,14 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
      rsync \
      git \
      tmux \
+     vim \
+     virtualbox-5.2 \
      linux-image-extra-$(uname -r) \
      linux-image-extra-virtual \
   && sudo usermod -aG docker $USER
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
   && sudo chmod +x /usr/local/bin/docker-compose
+bash <(curl -Ss https://raw.githubusercontent.com/dtulyakov/dotfiles/master/install.sh)
 
 #  && sudo apt-add-repository ppa:ansible/ansible -y
